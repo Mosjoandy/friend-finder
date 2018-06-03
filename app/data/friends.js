f9cbc31596a8cb0a92c86d50e7366f83ac6f363e
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-06-02 23:49:12 -0700

    make survey.html, try to post failing

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,34 +5,12 @@ var friends = [
     {
         name: "Ahmed",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-                ]
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         name: "Nancy",
         photo: "http://www.nanyanglaw.com/contents//images/Nancy.jpg",
-        scores: [
-            3,
-            1,
-            2,
-            3,
-            4,
-            5,
-            4,
-            3,
-            2,
-            1
-                ]
+        scores: [3, 1, 2, 3, 4, 5, 4, 3, 2, 1]
     }
 ]
 
--- app/server.js
+++ app/server.js
@@ -10,12 +10,24 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//////////////////////////show me the survey//////////////////////////////////
 
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/survey.html"));    
+});
+
+//////////////////////////show me the friendlist//////////////////////////////////
 
 app.get("/friends", function(req, res) {
-    // console.log(friends);
+    console.log(friends);
     return res.json(friends);
 });
+
+////////////////////////listener from survey page////////////////////////////////////
+
+
+
+////////////////////////whole list of friends////////////////////////////////////
 
 app.post("/friends", function(req, res) {
     res.send("You added a new friend")
@@ -27,6 +39,7 @@ app.post("/friends", function(req, res) {
 });
 
 
+////////////////////////End of the Line////////////////////////////////////
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
